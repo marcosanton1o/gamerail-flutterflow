58b7779613fb60b0dd00c652354fb507257957ff
marcosanton1o <marcos.lima221@aluno.ce.gov.br>
2025-04-30 22:03:43 -0300

    conexão com supabase concluida

---
+++ flutterflow/app/Providers/AppServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+
+public function boot()
+{
+
+    DB::statement("SET search_path TO public");
+}
+}
