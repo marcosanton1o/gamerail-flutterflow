1dec211f3b6e3632ec76dd34418ae37364fc706d
marcosanton1o <marcos.lima221@aluno.ce.gov.br>
2025-06-14 13:06:31 -0300

    edição de uma das tabelas de produto e arquivos JSON do postman

--- database/migrations/2025_06_02_144509_product.php
+++ database/migrations/2025_06_02_144509_product.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
+            $table->string('name');
             $table->text('description')->nullable();
             $table->string('image');
             $table->string('category');
